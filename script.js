395b432077fbbbd0d1acae1c8349189728cc8d6d
José Alves de Oliveira <160446328+professorjose@users.noreply.github.com>
2024-10-23 10:47:15 -0300

    ..

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function verificador(){
     const numero = document.getElementById('numero').value;
     const resultado = document.getElementById('resultado');
 
-    if(numero % 2 === 0)
+    if(numero % 2 === 0){
+        resultado.textContent = "Este número é par.";
+    } else {
+        resultado.textContent = "Este número é ímpar.";
+    }
 }
